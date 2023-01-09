fac4895a7b0b52e1d78bcf4211300a09b6724a1a
xiaoli <1184912869@qq.com>
2023-01-09 14:05:55 +0800

    手机电话卡的children显示

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -208,6 +208,7 @@ export default {
             box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
             z-index: 10;
             transition: all 0.5s;
+            background-color: #fff;
             .product {
               float: left;
               width: 16.6%;
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,233 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children">
+                <ul v-for="(item,index) in menuList" :key="index">
+                  <li v-for="(sub,sIndex) in item" :key="sIndex">
+                    <a :href="sub?'/#/product/'+sub.id:''">
+                      <img :src="sub?sub.img:'/imgs/item-box-1.png'">
+                      {{sub?sub.name:'小米CC9'}}
+                    </a>
+                  </li>
+                </ul>
+
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 平板</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插线板</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能 路由器</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">生活 箱包</a>
+              <div class="children"></div>
+            </li>
+          </ul>
+        </div>
+        <swiper :options="swiperOption" ref="mySwiper">
+          <swiper-slide v-for="(item, index) in slideList" :key="index">
+            <a :href="'/#/product/' + item.id">
+              <img :src="item.img" />
+            </a>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <!-- 导航按钮 -->
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
+    </div>
+    <service-bar></service-bar>
   </div>
 </template>
 
 <script>
+import ServiceBar from "@/components/ServiceBar.vue";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 export default {
-
-
-}
+  data() {
+    return {
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        effect: "cube",
+        cubeEffect: {
+          slideShadows: true,
+          shadow: true,
+          shadowOffset: 100,
+          shadowScale: 0.6,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg",
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg",
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg",
+        },
+        {
+          id: "48",
+          img: "/imgs/slider/slide-4.jpg",
+        },
+      ],
+      menuList: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9",
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版",
+          },
+          {
+            id: 32,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi k20 Pro",
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G+专区",
+          }
+        ],
+          [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+      ],
+    };
+  },
+  components: { Swiper, SwiperSlide, ServiceBar },
+};
 </script>
 
-<style>
-
+<style lang="scss">
+@import '../assets/scss/config.scss';
+@import "../assets/scss/mixin.scss";
+.index {
+  .swiper-box {
+    height: 451px;
+    .nav-menu {
+      position: absolute;
+      width: 264px;
+      height: 451px;
+      z-index: 9;
+      padding: 26px 0;
+      background-color: #55585a7a;
+      box-sizing: border-box;
+      padding: 26px 0;
+      .menu-wrap {
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+          a {
+            position: relative;
+            font-size: 16px;
+            color: #fff;
+            padding-left: 30px;
+            display: block;
+            &:after {
+              content: "";
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+              @include bgImg(10px, 15px, "../../public/imgs/icon-arrow.png");
+            }
+          }
+          &:hover {
+            background-color: #ff6600;
+            .children{
+              display: block;
+            }
+          }
+          .children{
+            display: none;
+            width: 1000px;
+            height: 451px;
+            background-color: #fff;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid #E5E5E5;
+            box-sizing: border-box;
+            ul{
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li{
+                height: 75px;
+                line-height: 75px;
+                flex:1;
+                padding-left: 23px;
+              }
+              a{
+                color: #333;
+                font-size: 14px;
+              }
+              img{
+                width: 42px;
+                height: 35px;
+                vertical-align: middle;
+                margin-right: 15px;
+              }
+            }
+          }
+        }
+      }
+    }
+    .swiper-container {
+      height: 451px;
+      .swiper-button-prev {
+        position: absolute;
+        left: 274px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
 </style>
