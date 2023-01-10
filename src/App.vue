307fb657675bbe4ffe62c85d5b4def6cafd8aab3
xiaoli <1184912869@qq.com>
2023-01-10 16:34:34 +0800

    product页面

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,14 @@ export default {
   },
   methods: {
     getUser() {
-      this.axios.get("/user").then((res) => {
+      // res={} 默认值是对象
+      this.axios.get("/user").then((res={}) => {
         this.$store.dispatch("saveUserName", res.username);
       });
     },
     getCartCount() {
-      this.axios.get("/carts/products/sum").then((res) => {
+      // res 默认是0
+      this.axios.get("/carts/products/sum").then((res=0) => {
         this.$store.dispatch("saveCartCount", res);
       });
     },
---
+++ src/components/ProductParam.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="nav-bar" :class="{is_fixed:isFixed}">
+    <div class="container">
+      <div class="pro-title">{{title}}</div>
+      <div class="pro-param">
+        <a href="">概述</a><span>|</span> <a href="">参数</a><span>|</span>
+        <a href="">用户评价</a>
+        <slot name="buy"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "nav-bar",
+  data() {
+    return {
+      isFixed: false,
+    };
+  },
+  props:['title'],
+  mounted() {
+    window.addEventListener('scroll',this.initHeight)
+  },
+  methods:{
+    initHeight(){
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+        this.isFixed = scrollTop > 152 ?true:false
+    }
+  },
+  destroyed(){
+    window.removeEventListener('scroll',this.initHeight,false)
+  }
+};
+</script>
+
+<style lang="scss" lang="scss">
+@import "@/assets/scss/config";
+@import "@/assets/scss/mixin";
+.nav-bar {
+  height: 70px;
+  line-height: 70px;
+  border-top: 1px solid #e5e5e5;
+  background-color: #fff;
+  &.is_fixed {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    box-shadow: 0 5px 5px $colorE;
+  }
+  .container {
+    @include flex();
+    .pro-title{
+      font-size: $fontH;
+      color: $colorB;
+      font-weight: bold;
+    }
+    .pro-param {
+      font-size: $fontJ;
+      a {
+        color: $colorC;
+      }
+      span {
+        margin: 0 10px;
+      }
+    }
+  }
+}
+</style>
