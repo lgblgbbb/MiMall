b30f8ce1d76fde15e15081e8ccef7b3831424848
xiaoli <1184912869@qq.com>
2023-01-11 18:01:00 +0800

    订单确认

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -170,7 +170,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "../assets/scss/config.scss";
 @import "../assets/scss/mixin.scss";
 .warn {
--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <order-header />
+    <order-header :title="title">
+      <template v-slot:tip> 
+        <span>{{tip}}</span>
+      </template>
+    </order-header>
     <router-view />
     <nav-footer />
   </div>
@@ -10,7 +14,26 @@
 import NavFooter from '@/components/NavFooter.vue'
 import OrderHeader from '@/components/OrderHeader.vue'
 export default {
-  components: { NavFooter, OrderHeader}
+  components: { NavFooter, OrderHeader},
+  data(){
+    return{
+      title:'',
+      tip:''
+    }
+  },
+  mounted(){
+    let path = this.$route.path
+    if(path == '/order/list'){
+      this.title = '订单列表'
+      this.tip = '谨慎防钓鱼链接或诈骗电话'
+    }else if(path == '/order/confirm'){
+       this.title = '订单确认'
+      this.tip = '请填写收货地址'
+    }else if(path == '/order/pay'){
+       this.title = '订单支付'
+      this.tip = '支付您的订单'
+    }
+  }
 
 }
 </script>
